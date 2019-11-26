003e15dbd11c572e96de8c73423dd2c55724beca
lmj01 <lmjie_good@163.com>
2019-11-26 16:45:43 +0800

    update

--- language/src/main/java/com/meijie/study/language/UseMap.java
+++ language/src/main/java/com/meijie/study/language/UseMap.java
@@ -1,6 +1,7 @@
 package com.meijie.study.language;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class UseMap {
@@ -19,17 +20,29 @@ public class UseMap {
 		map.setNotes(new HashMap<>());
 		Map<Integer, String> notes = map.getNotes();
 		notes.put(1, "val 1");
+		notes.put(20, "val 20");
+		notes.put(15, "val 15");
 		notes.put(2, "val 2");
 		int i = 0;
 		System.out.println("-------------");
 		notes.forEach((key,val)->{			
 			System.out.println(String.format("key is %s, value is %s", key, val));
 		});
+		
+		// 排序
+		Map<Integer, String> sorted = new LinkedHashMap<>();
+		notes.entrySet()
+			.stream()
+			.sorted(Map.Entry.comparingByKey())
+			.forEachOrdered(x -> sorted.put(x.getKey(), x.getValue()));
+		
 		System.out.println("-------------");
-		for (Integer key : notes.keySet()) {
+		for (Integer key : sorted.keySet()) {
 			i+=2;
 			System.out.println(String.format("key is %s, value is %s, %d", key, notes.get(key), i));
 		}
 		
 	}
+	
+
 }
